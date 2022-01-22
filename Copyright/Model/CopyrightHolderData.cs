9eab6a5e1a871e3e029817fa99351cdf92daa72c
Mikhail <Mikhail@HOME-PC>
2022-01-22 16:13:42 +0300

    22.01.2022

--- Copyright/Model/CopyrightHolderData.cs
+++ Copyright/Model/CopyrightHolderData.cs
@@ -8,5 +8,19 @@ namespace Copyright.Model
 {
     internal class CopyrightHolderData : ICopyrightHolderData
     {
+        public IEnumerable<CopyrightHolder> GetAllCopyrightHolders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CopyrightHolder GetCopyrightHolder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCopyrightHolder(int id, CopyrightHolder author)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
